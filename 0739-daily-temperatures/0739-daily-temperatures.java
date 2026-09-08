d7f41d310e57b2949185b9703908a7c685db5028
vtu29441 <vtu29441@veltech.edu.in>
2026-09-08 14:53:27 +0530

    Time: 24 ms (86.39%), Space: 104.3 MB (82.31%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -0,0 +1,21 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] answer = new int[n];
+        Deque<Integer> stack = new ArrayDeque<>(); // stores indices of temperatures
+
+        for (int i = 0; i < n; i++) {
+            // Process elements smaller than current temperature
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int prevIndex = stack.pop();
+                answer[prevIndex] = i - prevIndex;
+            }
+            stack.push(i);
+        }
+
+        return answer;
+    }
+}
